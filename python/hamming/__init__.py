40d05bb3bfa685ace79b58d378449ec6979d83b3
LilyBell <lily@annevar.com>
2017-07-29 10:15:56 -0400

    DNA Hamming computations solved for JavaScript and Python

--- python/hamming/__init__.py
+++ python/hamming/__init__.py
@@ -3,5 +3,17 @@
 # unit tests pass and your code meets all of the conditions.
 #
 
-def compute():
-    pass
+
+def compute(strand_x, strand_y):
+    x_len = len(strand_x)
+    y_len = len(strand_y)
+    dist = 0
+
+    if x_len != y_len:
+        raise ValueError("DNA strands must be of equal length.")
+    elif strand_x != strand_y:
+        for i in range(x_len):
+            if strand_x[i] != strand_y[i]:
+                dist += 1
+
+    return dist
