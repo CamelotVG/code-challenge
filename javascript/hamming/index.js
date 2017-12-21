5818ac0cfb6842b48f4852fdfdaaa1fdbf93eb84
beelarr <bryonl@me.com>
2017-12-21 11:26:15 -0600

    Add documentation to class and method

--- javascript/hamming/index.js
+++ javascript/hamming/index.js
@@ -3,7 +3,14 @@
 // your code meets all of the conditions.
 // You may use ES6 or ES5 to solve.
 
+
+
+// Class representing the Hamming Distance between two DNA strands.
 class Hamming {
+
+    // Computes the Hamming Distance.
+    // @param {string, string} strandOne - DNA strand. strandTwo - DNA strand.
+    // @returns {number} Number of differences
     compute(strandOne, strandTwo) {
         let hammingDistance = 0;
         if (strandOne.length !== strandTwo.length) {
