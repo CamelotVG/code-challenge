320074fe296c109e9598e73b0b63f93d1586d5c2
beelarr <bryonl@me.com>
2017-12-21 11:49:56 -0600

    Add for in loop zipping chars in arrays for comparison and returning sum of differences, two test are passing

--- python/hamming/__init__.py
+++ python/hamming/__init__.py
@@ -1,8 +1,17 @@
 # Define your compute function here.
 # run python -m unittest test.hamming_test to ensure the
 # unit tests pass and your code meets all of the conditions.
-#
+
+# Resources:
+# https://stackoverflow.com/questions/35328953/how-to-compare-individual-characters-in-two-strings-in-python-3
+# https://docs.python.org/3/library/functions.html#zip
+
+
+
 
 def compute(strand_one, strand_two):
     if len(strand_one) != len(strand_two):
         raise ValueError
+    else:
+        for nucleotides_one, nucleotides_two in zip(strand_one, strand_two):
+            return sum(nucleotides_one != nucleotides_two)
