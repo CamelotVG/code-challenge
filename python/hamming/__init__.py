f0a95be97a0e2ffa0bb46782aef4f460b62da9ab
beelarr <bryonl@me.com>
2017-12-21 11:41:42 -0600

    Add value error handler and two test are passing

--- python/hamming/__init__.py
+++ python/hamming/__init__.py
@@ -3,5 +3,6 @@
 # unit tests pass and your code meets all of the conditions.
 #
 
-def compute():
-    pass
+def compute(strand_one, strand_two):
+    if len(strand_one) != len(strand_two):
+        raise ValueError
